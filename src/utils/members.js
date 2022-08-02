989357f565c42543c1b0459eb09bffd5ffcb4741
ananth243 <raghavananth02@gmail.com>
2022-08-02 20:02:13 +0530

    Added a member

--- src/utils/members.js
+++ src/utils/members.js
@@ -1,6 +1,8 @@
 import ananth from '../assets/students/ananth.jpg';
 // import pravin from '../assets/faculty/pravin.min.png';
 import pravin from '../assets/faculty/pravin.jpeg';
+import indrani from '../assets/faculty/indrani.jpeg';
+import yash from '../assets/students/yash.jpeg';
 
 let student = [
   {
@@ -10,13 +12,25 @@ let student = [
     linkedIn: 'https://www.linkedin.com/in/ananth-raghav-2151a9200/',
     image: ananth,
   },
+  {
+    name: 'Yash Saini',
+    mobile: '+917742268889',
+    email: 'f20190973@goa.bits-pilani.ac.in',
+    linkedIn: 'https://www.linkedin.com/in/yash-saini-8a00b01a6',
+    image: yash,
+  },
 ];
 
 let faculty = [
   {
     image: pravin,
-    name: 'Dr. Pravine Mane',
+    name: 'Dr. Pravin Mane',
     url: 'https://universe.bits-pilani.ac.in/goa/pravinmane/profile',
+  },
+  {
+    image: indrani,
+    name: 'Dr. Indrani Talukdar',
+    url: 'https://universe.bits-pilani.ac.in/goa/indranit/profile',
   },
 ];
 
